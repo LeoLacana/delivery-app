3d2794b8bfa3e3c8c00e00ec592f1a5d57fc0c5a
Diego Ribeiro <dihrm2001@gmail.com>
2022-01-04 17:26:52 -0300

    Estrutura de rotas da aplicaçao iniciada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Navigate to="/login" /> } />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
