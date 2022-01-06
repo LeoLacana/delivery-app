743c8419eb1e912fb50450666a58e8697d78f91b
Erick-Marinho <erickgaves@gmail.com>
2022-01-06 17:53:23 -0300

    setup login route

--- back-end/src/routers/login.route.js
+++ back-end/src/routers/login.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const loginCtrl = require('../controllers/login.controller');
+const loginSchema = require('../schemas/login.schema');
+const validateSchemas = require('../middlewares/schemaValidate.middlewares');
 
 const router = express.Router();
 
 // /login
-// router.route('/').post(loginCtrl);
+router.route('/').post(validateSchemas(loginSchema), loginCtrl.login);
 
 module.exports = router;
