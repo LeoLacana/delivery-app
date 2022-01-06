1602c55e531d0e8ef9a38cb9e38e3658c6a61c73
Erick-Marinho <erickgaves@gmail.com>
2022-01-06 15:58:13 -0300

    implementing validate schemas in route

--- back-end/src/routers/register.route.js
+++ back-end/src/routers/register.route.js
@@ -1,11 +1,11 @@
-const { Users } = require('../database/models');
-const bcrypt = require('bcrypt');
 const express = require('express');
 const registerCtrl = require('../controllers/register.controller');
+const userSchema = require('../schemas/user.schema');
+const validateSchemas = require('../middlewares/schemaValidate.middlewares');
 
 const router = express.Router();
 
 // /register
-router.route('/').post(registerCtrl.create);
+router.route('/').post(validateSchemas(userSchema), registerCtrl.create);
 
 module.exports = router;
