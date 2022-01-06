aad8bd96d621603ef9ea1cae6a992d14aedbbc39
Erick-Marinho <erickgaves@gmail.com>
2022-01-06 15:56:49 -0300

    create schema validate

--- back-end/src/middlewares/schemaValidate.middlewares.js
+++ back-end/src/middlewares/schemaValidate.middlewares.js
@@ -1,9 +1,15 @@
-const validateSchema = (body, schema) => {
+const isValid = (body, schema) => {
   const result = schema.validate(body);
-  return result;
+  if (result.error) {
+    return result.error.details[0].message;
+  }
+  return null;
 };
 
-const validateBody = (schema) => (req, res, next) => {
-  const errorMessage = validateSchema(schema, req.body);
-  return console.log(errorMessage);
+const validateSchemas = (schema) => (req, res, next) => {
+  const errorMessage = isValid(req.body, schema);
+  if (!errorMessage) return next();
+  return res.status(400).json({ message: errorMessage });
 };
+
+module.exports = validateSchemas;
