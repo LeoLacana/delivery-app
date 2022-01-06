4bd5b6931f69b3b231665de6cd7755dae768ea44
Erick-Marinho <erickgaves@gmail.com>
2022-01-06 15:57:26 -0300

    implementing check name and email

--- back-end/src/models/register.model.js
+++ back-end/src/models/register.model.js
@@ -1,8 +1,15 @@
 const { Users } = require('../database/models');
 const bcrypt = require('bcrypt');
 const newToken = require('../auth/newToken.auth');
+const { Op } = require('sequelize');
 
 const create = async (name, email, password) => {
+  const checkUser = await Users.findOne({
+    where: {
+      [Op.or]: [{ name }, { email }]
+    }
+  });
+  if (checkUser) return null;
   const token = newToken(name, email, (role = 'customer'));
   const hash_password = await bcrypt.hash(password, 12);
   const user = await Users.create({
