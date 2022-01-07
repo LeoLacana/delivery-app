baeac6f49d5584e83ffc0b8f9bb22bea0e3851de
Diego Ribeiro <dihrm2001@gmail.com>
2022-01-07 15:44:51 -0300

    adiciona 'Provider' a aplicaçao para uso do Redux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={ <Navigate to="/login" /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/register" element={ <Register /> } />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={ <Navigate to="/login" /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/register" element={ <Register /> } />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
