079314bfa5200bbf19df9072cf0c328788f43195
Erick-Marinho <erickgaves@gmail.com>
2022-01-07 17:31:50 -0300

    setup model customer

---
+++ back-end/src/models/customer.model.js
@@ -0,0 +1,11 @@
+const { Products } = require('../database/models');
+
+const listProducts = async () => {
+  const products = await Products.findAll();
+  if (!products) return null;
+  return products;
+};
+
+module.exports = {
+  listProducts
+};
