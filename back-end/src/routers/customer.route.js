777974b0453bfed2eb0245379afcf3998ba310e0
Erick-Marinho <erickgaves@gmail.com>
2022-01-08 13:16:10 -0300

    delete comments

--- back-end/src/routers/customer.route.js
+++ back-end/src/routers/customer.route.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const customerCtrl = require('../controllers/customer.controller');
-// const userSchema = require('../schemas/user.schema');
-// const validateSchemas = require('../middlewares/schemaValidate.middlewares');
 
 const router = express.Router();
 
