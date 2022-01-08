ea12a89805efb13eb5bab604dda782a51cd0b7a2
Diego Ribeiro <dihrm2001@gmail.com>
2022-01-08 12:16:17 -0300

    corrige lint

--- front-end/src/redux/store/index.js
+++ front-end/src/redux/store/index.js
@@ -1,13 +1,6 @@
-import { createStore/* , applyMiddleware */ } from 'redux';
-// import thunk from 'redux-thunk';
-
+import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
-const store = createStore(
-  rootReducer,
-  /* eslint-disable no-underscore-dangle */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  /* eslint-enable */
-);
+const store = createStore(rootReducer);
 
 export default store;
