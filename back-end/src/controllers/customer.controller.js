e90d4cd8ed3048baf7226c78f74c44732817e7ae
Erick-Marinho <erickgaves@gmail.com>
2022-01-11 18:46:38 -0300

    setup customer controller - token verify

--- back-end/src/controllers/customer.controller.js
+++ back-end/src/controllers/customer.controller.js
@@ -1,11 +1,12 @@
 const customerModel = require('../models/customer.model');
+const verifyToken = require('../auth/verify.token');
 
 const listProducts = async (req, res) => {
   try {
     const products = await customerModel.listProducts();
     if (!products) {
       return res.status(409).json({
-        message: 'Produtos não encontrados',
+        message: 'Produtos não encontrados'
       });
     }
     return res.status(201).json(products);
@@ -15,8 +16,14 @@ const listProducts = async (req, res) => {
 };
 
 const createSale = async (req, res) => {
+  const token = req.headers.authorization;
+  let payload;
   try {
-    const saleId = await customerModel.createSale(req.body);
+    payload = verifyToken(token);
+    const saleId = await customerModel.createSale({
+      ...req.body,
+      user_id: payload.id
+    });
     return res.status(200).json({
       saleId
     });
@@ -25,7 +32,23 @@ const createSale = async (req, res) => {
   }
 };
 
+const getOrderById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const order = await customerModel.getOrderById(id);
+    if (!order) {
+      return res.status(409).json({
+        message: 'Venda não encontrada'
+      });
+    }
+    return res.status(200).json(order);
+  } catch (error) {
+    return res.status(500).json({ message: 'Erro interno no servidor' });
+  }
+};
+
 module.exports = {
   listProducts,
-  createSale
+  createSale,
+  getOrderById
 };
